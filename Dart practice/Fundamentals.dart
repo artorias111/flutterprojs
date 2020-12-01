1bbd81fdddf41bcfbc2462bafd48e639427e74ad
shriram <alekhshriram@gmail.com>
2020-12-01 11:37:46 +0530

    learning dart

---
+++ Dart practice/Fundamentals.dart
@@ -0,0 +1,9 @@
+void main(){
+    var s1='hello world';
+    dynamic s2='dynamic variable, can be an integer later';
+    print(s1);
+}
+
+String greet(String name){ //Function signature
+    return 'hellom $name';
+}
---
+++ Dart practice/what_is.md
@@ -0,0 +1,3 @@
+This is a playgroud for learning dart from flutterbyexample.com
+
+Continue from: https://flutterbyexample.com/lesson/const-and-final-variables
